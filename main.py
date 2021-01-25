b7be6eac255c13696f91170b8a1069a21eb706ba
Perchin <43394135+Perchin@users.noreply.github.com>
2021-01-25 14:11:20 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import enum
 import csv
 import matplotlib.pyplot as plt
 employees = []
@@ -14,11 +13,12 @@ class Person:
         self.gender = gender
         self.ipaddr = ipaddr
 
+        
 with open('MOCK_DATA.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     next(spamreader)
     for row in spamreader:
-        employees.append(Person(row[0],row[1],row[2],row[3],str(row[4]),row[5],str(row[6])))
+        employees.append(Person(row[0], row[1], row[2], row[3], str(row[4]), row[5], str(row[6])))
 
 
 def listEmployees(attr):
@@ -34,10 +34,11 @@ def listEmployees(attr):
 def printPerson(Person):
     print(Person.firstName + " " + str(Person.age) + ' ' + str(Person.email) + ' ' + str(Person.ipaddr))
 
+    
 def graph():
     names = ['Female', 'Male']
-    femaleCount = 0;
-    maleCount = 0;
+    femaleCount = 0
+    maleCount = 0
     for Person in employees:
         if Person.gender == 'Female':
             femaleCount = 1 + femaleCount
